89d8eb8eb424e6de06672650fb806db77205fa2a
Mahan Maleki <mahanmaleki950@gmail.com>
2025-09-26 00:00:53 +0330

    create an POST action in infoCrud.js file

--- modules/infoCrud.js
+++ modules/infoCrud.js
@@ -10,6 +10,27 @@ router.get('/info', async (req, res) => {
     res.json(showInfos);
 });
 
+router.post('/info', async (req, res) => {
+    const { desc, type } = req.body;
+
+    if (desc == undefined) {
+        return res.status(400).send("Bad Request: Please enter 'desc' value in request body.");
+    } else if (!desc || desc.trim() === "") {
+        return res.status(400).send("Bad Request: 'desc' cannot be empty or whitespace.");
+    }
+
+
+    try {
+        await prisma.info.create({
+            data: { desc, type }
+        });
+
+        const showInfos = await prisma.info.findMany();
+        res.json(showInfos);
+    } catch (error) {
+        res.status(500).send(`Server Error: ${error.message}`);
+    }
+});
 
 
 export default router;
