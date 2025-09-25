ed7849bb4ce62aa809703db34a3bae9f73cf4f7a
Mahan Maleki <mahanmaleki950@gmail.com>
2025-09-26 00:11:46 +0330

    create PUT action in infoCrud.js file

--- modules/infoCrud.js
+++ modules/infoCrud.js
@@ -32,5 +32,38 @@ router.post('/info', async (req, res) => {
     }
 });
 
+router.put('/info', async (req, res) => {
+    const { id, desc, type } = req.body;
+
+    if (id == undefined) {
+        return res.status(400).send("Bad Request: Please enter 'id' value in request body.");
+    } else if (!id) {
+        return res.status(400).send("Bad Request: 'id' cannot be empty or whitespace.");
+    } else if (desc == undefined) {
+        return res.status(400).send("Bad Request: Please enter 'desc' value in request body.");
+    } else if (!desc || desc.trim() === "") {
+        return res.status(400).send("Bad Request: 'desc' cannot be empty or whitespace.");
+    } else if (type == undefined) {
+        return res.status(400).send("Bad Request: Please enter 'type' value in request body.");
+    } else if (!type || type.trim() === "") {
+        return res.status(400).send("Bad Request: 'type' cannot be empty or whitespace.");
+    }
+
+    try {
+        await prisma.info.update({
+            where: {
+                id: id
+            }, data: {
+                desc: desc,
+                type: type
+            }
+        })
+
+        const showInfos = await prisma.info.findMany();
+        res.json(showInfos);
+    } catch (error) {
+        res.status(500).send(`Server Error: ${error.message}`);
+    }
+})
 
 export default router;
